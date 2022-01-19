310b7d6ba42981043fc3240798f5372d3e6cc7a5
sid_upadhyayula <s.upadhyayula1@ufl.edu>
2022-01-18 23:38:50 -0500

    Modifying UI

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+ 
+
+const Header = () => {
+    return (
+        <div className="UI Fixed Menu">
+            <div className="UI Container Center">
+                <h2>Contact Manager</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
