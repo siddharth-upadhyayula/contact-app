c545eeb52f38473e3ccd3b5f704c13bcd7b00ae0
sid_upadhyayula <s.upadhyayula1@ufl.edu>
2022-01-25 16:17:50 -0500

    UI

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -5,16 +5,18 @@ import React from "react";
 const ContactList = (props) => {
     console.log(props);
 
-    const renderContactList = props.contacts.map(contact) => {
+    const renderContactList = props.contacts.map((contact) => {
         return (
             <div className="item">
                 <div className="content">
-                    <div className="header"> name </div>
+                    <div className="header">{contact.name}</div>
+                    <div>{contact.email}</div>
                 </div>
+                <i className="Trash icon"></i>
             </div>
-        )
-    }
-    return <div className="UI List">ContactList</div>;
-}
+        );
+    });
+    return <div className="UI List">{renderContactList}</div>;
+};
 
 export default ContactList;
